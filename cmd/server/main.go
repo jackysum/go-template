c5004c95bd37a825b97187fe624f4c606091e61f
Jacky Sum <code@jsum.io>
2025-05-25 20:43:22 -0500

    add zerolog logger

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/pkgerrors"
+)
+
+func main() {
+	getLogger()
+}
+
+func getLogger() zerolog.Logger {
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+	zerolog.TimeFieldFormat = time.RFC3339Nano
+
+	out := zerolog.ConsoleWriter{
+		Out:        os.Stdout,
+		TimeFormat: time.RFC3339,
+	}
+
+	return zerolog.New(out).Level(zerolog.InfoLevel).With().Timestamp().Logger()
+}
